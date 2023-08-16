438ef85f63d56db77f08c2692c22f781246d862b
Jannis <2-jk@users.noreply.gl.hh01.jkirr.de>
2023-08-16 20:16:52 +0200

    feat: prisma init & docker compose

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GroupModule } from './group/group.module';
+import { ReceiptModule } from './receipt/receipt.module';
 
 @Module({
-  imports: [],
+  imports: [GroupModule, ReceiptModule],
   controllers: [AppController],
   providers: [AppService],
 })
