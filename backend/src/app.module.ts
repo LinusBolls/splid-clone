3993caed190783ff5329ed1a79f7131097989ddd
Florian König <koenidv@gmail.com>
2023-08-20 01:49:06 +0200

    chore: Create filestore service and controller

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GroupsModule } from './groups/groups.module';
 import { CurrenciesModule } from './currencies/currencies.module';
+import { FilestoreService } from './filestore/filestore.service';
+import { FilestoreController } from './filestore/filestore.controller';
 
 @Module({
   imports: [GroupsModule, CurrenciesModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, FilestoreController],
+  providers: [AppService, FilestoreService],
 })
 export class AppModule {}
